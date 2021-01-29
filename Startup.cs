123ffedfac40c10f19add59b4f6e3696be7c39af
smartin-hub <smartin@invallee.it>
2021-01-29 10:03:29 +0100

    commit di prova

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,6 @@ namespace Authentication
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
